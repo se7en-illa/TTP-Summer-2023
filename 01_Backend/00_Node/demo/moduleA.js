5cdef0f799104cf77c97973f081355f965e7cec6
Orlando Caraballo <orlandocaraballo@gmail.com>
2023-07-10 15:44:01 -0400

    Removed prettier from package.json file Added notes and demos

---
+++ 01_Backend/00_Node/demo/moduleA.js
@@ -0,0 +1,6 @@
+// here we execute the code in moduleB
+//  and retrieve the data from the module.exports of moduleB.js
+const moduleB = require("./moduleB.js");
+
+// we take the data from moduleB and display it in the terminal
+console.log(moduleB);
---
+++ 01_Backend/00_Node/demo/moduleB.js
@@ -0,0 +1,6 @@
+console.log("module B");
+
+module.exports = {
+  name: "Orlando",
+  age: 39,
+};
