0d29621b000c60372e72b2c6dcebf7971a399142
alexYea <alexanderterekhov1@gmail.com>
2023-07-26 07:19:59 -0400

    finished dynamic content, need to finish rest of lab

--- 02_Frontend/01_ReactJS/lab/react-intro/app/index.js
+++ 02_Frontend/01_ReactJS/lab/react-intro/app/index.js
@@ -1,1 +1,27 @@
 // Your code here!
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+function Counter() {
+    
+    const [count, setCount] = useState(0);
+    
+    function increment () {
+        setCount(count + 1);
+    };
+
+    return (
+        <div id="container">
+            <div id="navbar">Counter.js</div>
+                <div id="counter">
+                    <h1>{count}</h1>
+                    <button onClick={increment}>Increment</button>
+            </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+// root.render(<h1>Hello World!</h1>);
+root.render(<Counter />);
