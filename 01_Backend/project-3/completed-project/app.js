0028a0a7546aa6c37b70cd67152a91b5d29f4a64
alexYea <alexanderterekhov1@gmail.com>
2023-07-27 13:46:31 -0400

    modified+added new project-3 completed project files

--- 01_Backend/project-3/completed-project/app.js
+++ 01_Backend/project-3/completed-project/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+
 const app = express();
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
-
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`server is listening on port ${PORT}`);
 });
